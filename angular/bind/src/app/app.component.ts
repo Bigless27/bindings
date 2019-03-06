c7505f857628ef6345e95d84a0cda370791efc40
bigless27 <lessonryan@gmail.com>
2019-03-06 16:20:07 -0600

    complete angular bindings

---
+++ angular/bind/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'bind';
+
+  onewaybind = 'this is my one way bind';
+
+  inputValue = "";
+}
